b34e5dd2e7d38b90f9e30edb67a4351b3fe8941c
djumanov <djumanovdev@gmail.com>
2025-08-15 16:08:20 +0500

    inint

--- nt_pygrambot/handlers.py
+++
@@ -1,9 +0,0 @@
-from .types import Update
-
-class MessageHandler:
-
-    def __init__(self, callback: callable):
-        self.callback = callback
-
-    def check_update(self, update: Update):
-        return update.message is not None
